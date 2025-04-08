75e0e91036f206962f0471e6c73890a05901ebda
abarhub <abarhub@gmail.com>
2025-04-08 19:01:25 +0200

    utilisation de fonctions

--- devoxx.py
+++ devoxx.py
@@ -10,110 +10,123 @@ jour=1
 jour=2
 jour=3
 
-if jour==1:
-    file="devoxx1.html"
-    url0="https://www.devoxx.fr/agenda-2025/Schedule/"
-elif jour==2:
-    file="devoxx2.html"
-    url0 = "https://www.devoxx.fr/agenda-2025/Schedule/?id=Thursday"
-elif jour==3:
-    file="devoxx3.html"
-    url0 = "https://www.devoxx.fr/agenda-2025/Schedule/?id=Friday"
 
-contenu=""
+def recuperation_calendrier(jour:int):
+    if jour == 1:
+        file = "divers/devoxx1.html"
+        url0 = "https://www.devoxx.fr/agenda-2025/Schedule/"
+    elif jour == 2:
+        file = "divers/devoxx2.html"
+        url0 = "https://www.devoxx.fr/agenda-2025/Schedule/?id=Thursday"
+    elif jour == 3:
+        file = "divers/devoxx3.html"
+        url0 = "https://www.devoxx.fr/agenda-2025/Schedule/?id=Friday"
 
-a_dict = {}
+    contenu = ""
 
-my_file = Path(file)
-if my_file.is_file():
-    contenu=Path(my_file).read_text(encoding='utf-8')
-    #with open(file) as f:
-        #contenu = f.read()
-else:
-    # URL de l'agenda Devoxx 2025
-    url = url0
+    my_file = Path(file)
+    if my_file.is_file():
+        print(f"lecture fichier {file} sur le FS")
+        contenu=Path(my_file).read_text(encoding='utf-8')
+        #with open(file) as f:
+            #contenu = f.read()
+    else:
+        # URL de l'agenda Devoxx 2025
+        url = url0
+        print(f"lecture fichier {file} sur le web")
 
-    # Récupération de la page
-    response = requests.get(url)
+        # Récupération de la page
+        response = requests.get(url)
 
-    my_file.write_text(response.text, encoding='utf-8')
-    # with open("devoxx1.txt", "w") as text_file:
-    #     text_file.write(response.text)
-    contenu=response.text
+        my_file.write_text(response.text, encoding='utf-8')
+        # with open("devoxx1.txt", "w") as text_file:
+        #     text_file.write(response.text)
+        contenu=response.text
 
-soup = BeautifulSoup(contenu, 'html.parser')
+    return contenu
 
-# Extraction des conférences
-conferences = []
+def analyse_journee(jour:int):
 
-a_dict['liste'] = []
-a_dict['jour'] = jour
+    a_dict = {}
 
-# Les sessions sont dans des éléments avec la classe "schedule-day" (à vérifier)
-days = soup.find_all("div", class_="cfp-event",limit=500)  # Peut varier selon la structure HTML
 
-for day in days:
+    contenu=recuperation_calendrier(jour)
 
-    articles=day.find_all('article')
-    for article in articles:
-        #article = day.find('article')
+    soup = BeautifulSoup(contenu, 'html.parser')
 
-        event_name = article.find(class_='cfp-name').text.strip()
-        event_start = article['data-event-start']
-        event_finish = article['data-event-finish']
-        event_duration = article['data-event-duration']
+    # Extraction des conférences
+    conferences = []
 
-        lien=''
-        tmp=article.find(class_='cfp-a')
-        if tmp:
-            #lien=article.find(class_='cfp-a').attrs()['href'].text.strip()
-            lien = article.find(class_='cfp-a')['href']
+    a_dict['liste'] = []
+    a_dict['jour'] = jour
 
-        times = article.find_all('time')
-        datetime_start = times[0]['datetime']
-        datetime_end = times[1]['datetime']
+    # Les sessions sont dans des éléments avec la classe "schedule-day" (à vérifier)
+    days = soup.find_all("div", class_="cfp-event",limit=500)  # Peut varier selon la structure HTML
 
-        speaker=''
-        tmp=article.find(class_='cfp-speaker')
-        if tmp:
-            speaker=tmp.text.strip()
+    for day in days:
 
-        favori=-1
-        tmp = article.find(class_='cfp-favourite')
-        if tmp:
-            favori = int(tmp.text.strip())
+        articles=day.find_all('article')
+        for article in articles:
+            #article = day.find('article')
 
-        # if event_start.find("T")>=0:
-        #     event_start=event_start[event_start.find('T')+1:]
-        # if event_start.rfind("+")>=0:
-        #     event_start=event_start[:event_start.rfind('+')]
+            event_name = article.find(class_='cfp-name').text.strip()
+            event_start = article['data-event-start']
+            event_finish = article['data-event-finish']
+            event_duration = article['data-event-duration']
 
-        print("Nom :", event_name)
-        print("Début :", event_start)
-        print("Fin :", event_finish)
-        print("Durée :", event_duration)
-        print("Datetime start :", datetime_start)
-        print("Datetime end :", datetime_end)
-        print("speaker :", speaker)
-        print("favori :", favori)
-        print("lien :", lien)
+            lien=''
+            tmp=article.find(class_='cfp-a')
+            if tmp:
+                #lien=article.find(class_='cfp-a').attrs()['href'].text.strip()
+                lien = article.find(class_='cfp-a')['href']
 
-        dict2={}
-        dict2['event_name'] = event_name
-        dict2['event_start'] = event_start
-        dict2['event_finish'] = event_finish
-        dict2['event_duration'] = event_duration
-        dict2['datetime_start'] = datetime_start
-        dict2['datetime_end'] = datetime_end
-        dict2['speaker'] = speaker
-        dict2['favori'] = favori
-        dict2['lien'] = lien
-        a_dict['liste'].append(dict2)
+            times = article.find_all('time')
+            datetime_start = times[0]['datetime']
+            datetime_end = times[1]['datetime']
 
-        if len(article.find_all(class_='cfp-name'))>1:
-            nom2=article.find_all(class_='cfp-name')[1].text.strip()
-            print("Nom2 :", nom2)
-            dict2['nom'] = nom2
+            speaker=''
+            tmp=article.find(class_='cfp-speaker')
+            if tmp:
+                speaker=tmp.text.strip()
+
+            favori=-1
+            tmp = article.find(class_='cfp-favourite')
+            if tmp:
+                favori = int(tmp.text.strip())
+
+            # if event_start.find("T")>=0:
+            #     event_start=event_start[event_start.find('T')+1:]
+            # if event_start.rfind("+")>=0:
+            #     event_start=event_start[:event_start.rfind('+')]
+
+            print("Nom :", event_name)
+            print("Début :", event_start)
+            print("Fin :", event_finish)
+            print("Durée :", event_duration)
+            print("Datetime start :", datetime_start)
+            print("Datetime end :", datetime_end)
+            print("speaker :", speaker)
+            print("favori :", favori)
+            print("lien :", lien)
+
+            dict2={}
+            dict2['event_name'] = event_name
+            dict2['event_start'] = event_start
+            dict2['event_finish'] = event_finish
+            dict2['event_duration'] = event_duration
+            dict2['datetime_start'] = datetime_start
+            dict2['datetime_end'] = datetime_end
+            dict2['speaker'] = speaker
+            dict2['favori'] = favori
+            dict2['lien'] = lien
+            a_dict['liste'].append(dict2)
+
+            if len(article.find_all(class_='cfp-name'))>1:
+                nom2=article.find_all(class_='cfp-name')[1].text.strip()
+                print("Nom2 :", nom2)
+                dict2['nom'] = nom2
+
+    return a_dict
 
     # date_str = day.find("h2").get_text().strip()  # Ex: "Mercredi 9 Avril 2025"
     # date_obj = datetime.strptime(date_str, "%A %d %B %Y").date()  # Formatage de la date
@@ -152,5 +165,18 @@ for day in days:
 
 #print("Export terminé : devoxx_2025_conferences.csv")
 
-with open('docs/data_'+str(jour)+'.json', 'w') as outfile:
-    json.dump(a_dict, outfile,indent=4)
+dict1=analyse_journee(1)
+dict2=analyse_journee(2)
+dict3=analyse_journee(3)
+
+if dict1 is None:
+    raise "Erreur pour le jour 1"
+if dict2 is None:
+    raise "Erreur pour le jour 2"
+if dict3 is None:
+    raise "Erreur pour le jour 3"
+
+dict_complet= {'liste1': dict1['liste'], 'liste2': dict2['liste'], 'liste3': dict3['liste']}
+
+with open('docs/data_complet.json', 'w') as outfile:
+    json.dump(dict_complet, outfile,indent=4)
